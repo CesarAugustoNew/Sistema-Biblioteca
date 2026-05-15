41058e95a504018477567023552b14a557f90d3d
Cesar Augusto <cesarprofissional22@gmail.com>
2026-05-15 01:07:50 -0300

    conexão com banco de dados feita com sucesso

---
+++ SistemaBiblioteca/src/main/java/Database/sqlConn.java
@@ -0,0 +1,31 @@
+package Database;
+
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+public class sqlConn {
+    private static final String url = "jdbc:mysql://localhost:3306/sistema_biblioteca";
+    private static final String user = "root";
+    private static final String password = "Cesar123";
+
+    public static Connection getConnection() throws SQLException
+    {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    // VERIFICANDO CONEXÃO
+    public static void testeConnction() {
+        try(Connection conn = getConnection()){
+            System.out.println("Conexão foi bem estabelecida com sucesso!" + conn);
+        } catch (SQLException e){
+            System.out.println("falha na conexão" + e.getMessage());
+            System.out.println("verifique: ");
+            System.out.println("1. MySql está rodando? ");
+            System.out.println("2. O banco " + url + "realmente existe? ");
+            System.out.println("3. O usuário ou senha estão corretos?");
+        }
+
+    }
+}
--- SistemaBiblioteca/src/main/java/org/example/App.java
+++ SistemaBiblioteca/src/main/java/org/example/App.java
@@ -1,13 +1,9 @@
 package org.example;
+import Database.sqlConn;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        sqlConn.testeConnction();
     }
 }
